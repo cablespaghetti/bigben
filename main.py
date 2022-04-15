fb3e9ab9e2ecf3e456ed12fdb9552cea4c2ac2c3
Sam Weston <sam.weston@tails.com>
2022-04-15 23:20:58 +0100

    I really need to write tests for this...

--- main.py
+++ main.py
@@ -5,29 +5,28 @@ import pytz
 
 
 def handler(event, context):
-    access_token = os.environ.get('MASTODON_ACCESS_TOKEN', '')
-    api_base_url = os.environ.get('MASTODON_BASE_URL', '')
+    access_token = os.environ.get("MASTODON_ACCESS_TOKEN", "")
+    api_base_url = os.environ.get("MASTODON_BASE_URL", "")
 
     if not access_token or not api_base_url:
-        print('You must set both a MASTODON_ACCESS_TOKEN and MASTODON_BASE_URL environment variable')
+        print(
+            "You must set both a MASTODON_ACCESS_TOKEN and MASTODON_BASE_URL environment variable"
+        )
         exit(1)
 
-    mastodon = Mastodon(
-        access_token=access_token,
-        api_base_url=api_base_url
-    )
-    now = datetime.datetime.now(pytz.timezone('Europe/London'))
+    mastodon = Mastodon(access_token=access_token, api_base_url=api_base_url)
+    now = datetime.datetime.now(pytz.timezone("Europe/London"))
     hour = now.hour % 12
 
-    if now.month == 1 and now.day == 1:
+    if now.month == 1 and now.day == 1 and now.hour == 0:
         filename = "newyear.jpg"
     else:
-        filename = f'{hour}.jpg'
+        filename = f"{hour}.jpg"
 
     print(filename)
 
-    photo = mastodon.media_post('images/' + filename, "image/jpeg")
-    mastodon.status_post(('BONG ' * hour).rstrip(), media_ids=photo)
+    photo = mastodon.media_post("images/" + filename, "image/jpeg")
+    mastodon.status_post(("BONG " * hour).rstrip(), media_ids=photo)
 
 
 if __name__ == "__main__":
