5f4522f75a34f222f9da70fe7650171dd1950c50
Sam Weston <sam.weston@tails.com>
2022-04-17 23:16:38 +0100

    Write some iffy tests, find a bug...shocking!

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import os
 import pytz
 
 
-def get_mastodon():
+def get_mastodon() -> Mastodon:
+    """Creates an instance of the Mastodon Class
+
+    Returns:
+        Mastodon: The instance of the Mastodon Class
+    """
     access_token = os.environ.get("MASTODON_ACCESS_TOKEN", "")
     api_base_url = os.environ.get("MASTODON_BASE_URL", "")
 
@@ -17,13 +22,28 @@ def get_mastodon():
     return Mastodon(access_token=access_token, api_base_url=api_base_url)
 
 
-def get_time_in_london():
+def get_time_in_london() -> tuple[int, int, int]:
+    """Gets the current time in London right now
+
+    Returns:
+        tuple[int, int, int]: A tuple with the hour (12 hour clock), day of the month and month of the year
+    """
     now = datetime.datetime.now(pytz.timezone("Europe/London"))
-    return now.hour % 12, now.day, now.month
+    return int(now.strftime("%I")), now.day, now.month
 
 
-def get_big_ben_image_path(hour, day, month):
-    if month == 1 and day == 1 and hour == 0:
+def get_big_ben_image_path(hour: int, day: int, month: int) -> str:
+    """Gets an image of Big Ben given the time
+
+    Args:
+        hour (int): The hour in London (12 hour clock)
+        day (int): The day of the month in London
+        month (int): The month of the year in London
+
+    Returns:
+        str: The relative path of a JPEG image
+    """
+    if month == 1 and day == 1 and hour == 12:
         filename = "newyear.jpg"
     else:
         filename = f"{hour}.jpg"
@@ -31,7 +51,13 @@ def get_big_ben_image_path(hour, day, month):
     return "images/" + filename
 
 
-def post_to_mastodon(image_path, hour):
+def post_to_mastodon(image_path: str, hour: int):
+    """Post to Mastodon
+
+    Args:
+        image_path (str): The path of a JPEG image
+        hour (int): The time to bong for
+    """
     mastodon = get_mastodon()
     photo = mastodon.media_post(image_path, "image/jpeg")
     mastodon.status_post(("BONG " * hour).rstrip(), media_ids=photo)
--- test_main.py
+++ test_main.py
@@ -1,4 +1,6 @@
 from unittest import TestCase, mock
+from freezegun import freeze_time
+
 import os
 
 from main import handler
@@ -16,10 +18,41 @@ class BigBenTestSuite(TestCase):
         {"MASTODON_ACCESS_TOKEN": "foo", "MASTODON_BASE_URL": "banana.com"},
     )
     @mock.patch("main.Mastodon")
+    @freeze_time("2000-01-02 22:02:02")
     def test_happy_case(self, FakeMastodon):
         mastodon = FakeMastodon()
         handler(None, None)
         mastodon.media_post.assert_called_once_with("images/10.jpg", "image/jpeg")
         mastodon.status_post.assert_called_once_with(
             "BONG BONG BONG BONG BONG BONG BONG BONG BONG BONG", media_ids=mock.ANY
+        )
+
+    @mock.patch.dict(
+        os.environ,
+        {"MASTODON_ACCESS_TOKEN": "foo", "MASTODON_BASE_URL": "banana.com"},
+    )
+    @mock.patch("main.Mastodon")
+    @freeze_time("2000-01-02 00:02:02")
+    def test_midnight_case(self, FakeMastodon):
+        mastodon = FakeMastodon()
+        handler(None, None)
+        mastodon.media_post.assert_called_once_with("images/12.jpg", "image/jpeg")
+        mastodon.status_post.assert_called_once_with(
+            "BONG BONG BONG BONG BONG BONG BONG BONG BONG BONG BONG BONG",
+            media_ids=mock.ANY,
+        )
+
+    @mock.patch.dict(
+        os.environ,
+        {"MASTODON_ACCESS_TOKEN": "foo", "MASTODON_BASE_URL": "banana.com"},
+    )
+    @mock.patch("main.Mastodon")
+    @freeze_time("2000-01-01 00:02:02")
+    def test_happy_new_year_case(self, FakeMastodon):
+        mastodon = FakeMastodon()
+        handler(None, None)
+        mastodon.media_post.assert_called_once_with("images/newyear.jpg", "image/jpeg")
+        mastodon.status_post.assert_called_once_with(
+            "BONG BONG BONG BONG BONG BONG BONG BONG BONG BONG BONG BONG",
+            media_ids=mock.ANY,
         )
