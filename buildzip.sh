a3c43b07b3ab355e31854092a95117f69e188f49
Sam Weston <sam.weston@tails.com>
2022-04-18 19:19:06 +0100

    Use Pipenv

--- buildzip.sh
+++ buildzip.sh
@@ -3,7 +3,8 @@ rm -f bigben.zip
 rm -rf venv
 virtualenv --python=python3.9 venv
 source venv/bin/activate
-pip3 install -r requirements.txt
+pip install pipenv
+pipenv install --ignore-pipfile
 deactivate
 cd venv/lib/python3.9/site-packages/
 zip -r9 ../../../../bigben.zip .
