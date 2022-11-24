0a567e3c1470dc5f0fa8e3829b75931d3b8e0b81
Sam Weston <11150054+cablespaghetti@users.noreply.github.com>
2022-11-24 10:22:33 +0000

    Fix deploy for pipenv

--- buildzip.sh
+++ buildzip.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
+#!/bin/bash -ex
 rm -f bigben.zip
 rm -rf venv
+pipenv requirements > requirements.txt
 virtualenv --python=python3.9 venv
 source venv/bin/activate
-pip install pipenv
-pipenv install --ignore-pipfile
+pip3 install -r requirements.txt
 deactivate
 cd venv/lib/python3.9/site-packages/
 zip -r9 ../../../../bigben.zip .
 cd ../../../../
 zip -g bigben.zip -r *.py images
-
